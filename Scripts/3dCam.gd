f055168db0252b2ec8395d458ec3ac044ca5c5f1
Smg065 <tails2tails@gmail.com>
2025-09-26 22:09:16 -0600

    CamTiltTyping

--- Scripts/3dCam.gd
+++ Scripts/3dCam.gd
@@ -2,9 +2,15 @@ extends Camera3D
 class_name PlayCam
 
 ##The rotation when you're typing
-@export var typingRot : Quaternion
+@export var typingRot : float = -12.0
 ##The rotation when you're watching
-@export var staticRot : Quaternion
+@export var staticRot : float = 0
+
+var lastTypedCooldown : float = 0
 
 func _process(delta: float) -> void:
-	
+	var targetRot := staticRot
+	if lastTypedCooldown > 0:
+		lastTypedCooldown -= delta
+		targetRot = typingRot
+	rotation_degrees.x = lerp_angle(rotation_degrees.x, targetRot, delta)
