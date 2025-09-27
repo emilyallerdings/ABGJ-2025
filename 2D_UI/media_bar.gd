a898cda29f2f6ddf640c119a180ce1f14e1b05e2
emilyallerdings <skittlesanator@gmail.com>
2025-09-27 16:03:09 -0600

    audio player functionality

--- 2D_UI/media_bar.gd
+++ 2D_UI/media_bar.gd
@@ -2,11 +2,17 @@ extends TextureRect
 
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 @export var current_media:SoundGroup
+@onready var media_handle: Sprite2D = $MediaHandle
 
 @onready var total_time_label: Label = $TotalTimeLabel
 @onready var cur_time_label: Label = $CurTimeLabel
 
 var cur_audio_stream:AudioStream
+
+var media_handle_start = 12.0
+var media_handle_end = 237.0
+
+var paused_playback_pos:float = 0.0
 
 func setup_media(current_media:SoundGroup = current_media):
 	cur_audio_stream = current_media.soundFiles.get(0)
@@ -17,11 +23,27 @@ func setup_media(current_media:SoundGroup = current_media):
 	audio_stream_player.stream = cur_audio_stream
 	cur_time_label.text = "0.00"
 	total_time_label.text = format_time(cur_audio_stream.get_length())
-
+	media_handle.position.x = media_handle_start
+	paused_playback_pos = 0.0
+	
 func _process(delta: float) -> void:
+	
+	
 	cur_time_label.text = format_time(audio_stream_player.get_playback_position())
-
+	var normalized_dur = audio_stream_player.get_playback_position() / cur_audio_stream.get_length()
+	if audio_stream_player.playing:
+		media_handle.position.x = media_handle_start + normalized_dur*media_handle_end
+	
 func format_time(time: float) -> String:
 	var seconds = int(time)
 	var milliseconds = int((time - seconds) * 100)
 	return "%d.%02d" % [seconds, milliseconds]
+
+
+func _on_play_media_btn_pressed() -> void:
+	if !audio_stream_player.playing && !audio_stream_player.stream_paused:
+		audio_stream_player.playing = true
+	else:
+		
+		audio_stream_player.stream_paused = !audio_stream_player.stream_paused
+	pass # Replace with function body.
