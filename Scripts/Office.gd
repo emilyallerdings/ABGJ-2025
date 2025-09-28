9258ec4c5d553d09bc1d2c189b6fac949dc06fa4
Smg065 <tails2tails@gmail.com>
2025-09-28 12:57:52 -0600

    MaxCorruptionFunciton

--- Scripts/Office.gd
+++ Scripts/Office.gd
@@ -27,10 +27,17 @@ func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("Pause") and pauseMenu.visible == false:
 		pauseMenu.set_paused(true)
 
+static func increase_corruption(corruptIncrease : float):
+	MaxCorruption = clampf(corruptIncrease + MaxCorruption, 0, 1)
+
 func timer_bump():
 	progressionLevel += 1.0#randf_range(0.05,0.005)
 	progressionLevel = clamp(progressionLevel, 0, MaxCorruption)
 	new_corruption_level.emit(progressionLevel)
+
+func try_random_noise():
+	if randi_range(0, 20) == 0:
+		%RandomNoise.play()
 
 func power_toggle():
 	PoweredOn = !PoweredOn
@@ -38,6 +45,6 @@ func power_toggle():
 
 func gain_strike():
 	%StrikeSound.play()
-	MaxCorruption += 0.1
+	increase_corruption(0.1)
 	Persist.strikes += 1
 	strike_gained.emit(Persist.strikes)
