e0429df143d5a7cc23a79cec379583286e9842de
emilyallerdings <skittlesanator@gmail.com>
2025-09-27 21:44:13 -0600

    audio connection to ui

--- 2D_UI/main_screen.gd
+++ 2D_UI/main_screen.gd
@@ -6,7 +6,7 @@ extends Control
 
 const LEAF_CRUNCH = preload("uid://c7nad47mbe4a7")
 
-
+var current_media:SoundGroup
 
 func _process(delta: float) -> void:
 	$Cursor.position = get_local_mouse_position()
@@ -17,9 +17,14 @@ func _ready() -> void:
 	return
 
 func set_media(media:SoundGroup):
+	current_media = media
 	media_bar.setup_media(media)
 
 func _on_submit_btn_pressed() -> void:
+	var guess = %GuessInput.text
+	var pass_result = Persist.pass_guess(current_media.soundName, guess)
+	if !pass_result.passed:
+		%ErrorPopupWindow.visible = true
 	pass # Replace with function body.
 
 
@@ -27,4 +32,8 @@ func _on_next_button_pressed() -> void:
 	welcome.visible = false
 	audio_processing.visible = true
 	set_media(LEAF_CRUNCH)
+	pass # Replace with function body.
+
+
+func _on_ok_btn_pressed() -> void:
 	pass # Replace with function body.
